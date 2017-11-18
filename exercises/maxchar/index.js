157c6d826563459efa8d6041ba7938b22685c701
Aleksandr <alx.panteleev@gmail.com>
2017-11-18 19:12:16 +0300

    using character map for solving maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = (str) => {
+    const charsObject = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charsObject[char] = charsObject[char] ? charsObject[char]++ : 1
+    }
+
+    for (let char in charsObject) {
+        if (charsObject[char] > max) {
+            max = charsObject[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar
+};
 
 module.exports = maxChar;
