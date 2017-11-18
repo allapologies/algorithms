3415f68128a9bc393405ba3ab34d374383cbd3b9
Aleksandr <alx.panteleev@gmail.com>
2017-11-18 17:23:44 +0300

    add eslint

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,13 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+
+// 3
+const reverse = () => ({})
+
+module.exports = reverse;
