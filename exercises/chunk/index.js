d8303034368cbcccfb50880cf6171b574e8e4b56
Aleksandr <alx.panteleev@gmail.com>
2017-11-18 21:51:24 +0300

    chunked solution using last element

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,14 +10,32 @@
 
 const chunk = (array, size) => {
     const chunked = [];
-    let index = 0;
 
-    while (index < array.length) {
-        chunked.push(array.slice(index, index + size))
-        index += size
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+
+        if(!last || last.length === size) {
+            chunked.push([element])
+        } else {
+            last.push(element)
+        }
     }
 
-    return chunked
+    return chunked;
 };
+
+
+
+// const chunk = (array, size) => {
+//     const chunked = [];
+//     let index = 0;
+//
+//     while (index < array.length) {
+//         chunked.push(array.slice(index, index + size))
+//         index += size
+//     }
+//
+//     return chunked
+// };
 
 module.exports = chunk;
