3aecf5e76afc76e34b9631dac77b7ced341c7581
aleksandr <alx.panteleev@gmail.com>
2018-04-08 12:27:56 +0300

    chunked: solution with slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,21 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-// 2
+// 3
 const chunk = (array, size) => {
   const chunked = [];
-  for (const element of array) { // eslint-disable-line
-    const last = chunked[chunked.length - 1];
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
   return chunked;
 };
 
+
 module.exports = chunk;
+
+
+// 2
+// const chunk = (array, size) => {
+//   const chunked = [];
+//   for (const element of array) { // eslint-disable-line
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// };
+//
 
 // 1
 // const chunk = (array, size) => {
